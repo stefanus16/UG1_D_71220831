31b06a0f57cff2f630c4a002d27caf1b9cdb6d2c
stefanus16 <117900966+stefanus16@users.noreply.github.com>
2023-02-08 19:34:40 +0700

    Add files via upload

---
+++ 1_D_71220831.py
@@ -0,0 +1,10 @@
+print("==================== kasir ====================")
+a = int(input("Harga barang:"))
+Nota =  0
+
+pilihan=int(input("apakah anda ingin membeli barang lagi?[yes/no]: "))
+if(pilihan):
+    input("Harga barang:")
+    input("apakah anda ingin membeli barang lagi?[yes/no]: ")
+if(pilihan):
+    input("total belanja:")
